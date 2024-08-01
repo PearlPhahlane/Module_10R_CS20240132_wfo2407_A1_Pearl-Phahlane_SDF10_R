372d21bafccd0be1dbb4b7354870adb9a10118b7
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-08-01 15:59:59 +0200

    create new firebase realtime dadabase

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js"
+
+
+const firebaseConfig = {
+    databaseURL: "https://leads-tracker-app-563e4-default-rtdb.europe-west1.firebasedatabase.app/"
+
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+
+console.log(database)
+
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
