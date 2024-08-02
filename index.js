bd118ea2a61579e396588882a4cbf1c31561ca3b
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-08-02 11:38:32 +0200

    use innerhtml to add items to the list on the page

--- index.js
+++ index.js
@@ -12,11 +12,15 @@ const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
 const shoppingListInDB = ref (database, "shoppingList")
 
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
     push(shoppingListInDB, inputValue)
+
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
 })
